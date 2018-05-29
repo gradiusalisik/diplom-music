6e12c1a15601feae5e17d0e9687d9faeb378efbe
Vladislav <gradiusalisik@mail.ru>
2018-05-29 23:18:38 +0300

    add magic

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import Drift from 'drift-zoom';
 
 import Button from '../blocks/button/button';
 import Main from '../blocks/main/main';
@@ -14,6 +15,24 @@ $(window).on('click', () => {
 
 	$audio[0].play();
 });
+
+const demoTrigger = document.querySelector('.js-image');
+const demoTrigger2 = document.querySelector('.js-image2');
+const paneContainer = document.querySelector('.js-for-zoom-photo');
+
+if (demoTrigger && paneContainer) {
+	new Drift(demoTrigger, { // eslint-disable-line
+		paneContainer: paneContainer,  // eslint-disable-line
+		inlinePane: false
+	});
+}
+
+if (demoTrigger2 && paneContainer) {
+	new Drift(demoTrigger2, { // eslint-disable-line
+		paneContainer: paneContainer,  // eslint-disable-line
+		inlinePane: false
+	});
+}
 
 $(() => {
 	svg4everybody();
